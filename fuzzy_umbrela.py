c2e5cc8055b8d031640c77903526721992a44410
JulioNascimento500 <57997136+JulioNascimento500@users.noreply.github.com>
2022-10-14 13:31:57 +0100

    Update fuzzy_umbrela.py

--- fuzzy_umbrela.py
+++ fuzzy_umbrela.py
@@ -5,7 +5,7 @@ import fuzzy_umbrela_functions
 if __name__ == '__main__':
 
 
-    lista          = ['PBAND_A11_UP.dat']#glob.glob('PBAND*')
+    lista          = ['PBAND_A11_UP.dat']#glob.glob('./band-file/PBAND*')
 
     DataBase = fuzzy_umbrela_functions.readDataBase(lista)
 
