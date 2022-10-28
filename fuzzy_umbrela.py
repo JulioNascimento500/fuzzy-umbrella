b2dbb57341804eae1cda9c21193d6aeed1cc4449
JulioNascimento500 <57997136+JulioNascimento500@users.noreply.github.com>
2022-10-28 10:14:35 +0100

    Update fuzzy_umbrela.py

--- fuzzy_umbrela.py
+++ fuzzy_umbrela.py
@@ -16,6 +16,6 @@ if __name__ == '__main__':
     ListOrbitals   = ['s','py','pz','px','dxy','dyz','dz2','dxz','x2-y2','fy3x2','fxyz','fyz2','fz3','fxz2','fzx2','fx3','tot']
     fileName       = './file.html'
 
-    fuzzy_umbrela_functions.plotEnergies(DataBase,Atoms,SpinCh,ListOrbitals,fileName='./fileEnergy.html',marker_Multiplier=10)
+    fuzzy_umbrela_functions.plotEnergies(DataBase,Atoms,SpinCh,ListOrbitals,fileName='./fileEnergy.html',opacity=0.5)
     fuzzy_umbrela_functions.plotBands(DataBase,Atoms,SpinCh,ListOrbitals,fileName='./file.html')
-    fuzzy_umbrela_functions.plotBands(DataBase,Atoms,SpinCh,ListOrbitals,fileName='./fileBands.html',marker_Multiplier=10)
+    fuzzy_umbrela_functions.plotBandsWeight(DataBase,Atoms,SpinCh,ListOrbitals,fileName='./fileBands.html')
